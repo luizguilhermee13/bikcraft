906c8b9e0394355ec01fc633e98dcae7597a8d79
luizguilhermee13 <luizguidelila@gmail.com>
2026-08-01 21:34:29 -0300

    page orcamentos concluido

---
+++ assets/css/orcamentos/orcamentos.js
@@ -0,0 +1,27 @@
+function mostrarBikecraft() {
+  document.getElementById("orcamento-bikcraft").style.display = "grid";
+  document.getElementById("orcamento-seguro").style.display = "none";
+}
+
+function mostrarSeguro() {
+  document.getElementById("orcamento-seguro").style.display = "grid";
+  document.getElementById("orcamento-bikcraft").style.display = "none";
+}
+
+function mostrarNimbus() {
+  document.getElementById("nimbus1").style.display = "grid";
+  document.getElementById("magic1").style.display = "none";
+  document.getElementById("nebula1").style.display = "none";
+  d;
+}
+
+function mostrarMagic() {
+  document.getElementById("magic1").style.display = "grid";
+  document.getElementById("nimbus1").style.display = "none";
+  document.getElementById("nebula1").style.display = "none";
+}
+function mostrarNebula() {
+  document.getElementById("nebula1").style.display = "grid";
+  document.getElementById("nimbus1").style.display = "none";
+  document.getElementById("magic1").style.display = "none";
+}
