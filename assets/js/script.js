19f7fe1726df03f9bc089c48d44467acef7bc141
luizguilhermee13 <luizguidelila@gmail.com>
2026-08-11 18:57:56 -0300

    implementando plugin e js em seguros.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,45 @@ function ativarProduto(parametros) {
 }
 
 parametro.forEach(ativarProduto);
+
+//Perguntas Frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function selecaoPergunta(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(selecaoPergunta);
+
+//trocando imagens
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function Galeria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(Galeria);
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
